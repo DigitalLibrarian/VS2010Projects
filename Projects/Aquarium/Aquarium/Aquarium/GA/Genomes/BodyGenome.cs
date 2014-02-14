afbbc38e9dc992ed3862f06e559b2f235816746c
Adam Corum <DigitalLibrarian@github.com>
2014-02-13 22:32:18 -0600

    Better mutations
    * Changed mutations so that one  and only one is picked at random,
    rather than some random subset.  This seems to make it much more precise
    it the exploration.
    * Faster convergence

--- Projects/Aquarium/Aquarium/Aquarium/GA/Genomes/BodyGenome.cs
+++ Projects/Aquarium/Aquarium/Aquarium/GA/Genomes/BodyGenome.cs
@@ -11,72 +11,66 @@ namespace Aquarium.GA.Genomes
 
         public override void Mutate(Random random)
         {
-            int chance = 10;
             var geneIndex = random.Next(Genes.Count());
             var gene = Genes[geneIndex];
-
-            if (random.Next(chance) == 0)
+            var mutators = new List<Action>
             {
-                var newGene = new Gene<int>
-                {
-                    Name = gene.Name + 1,
-                    Value = gene.Value
-                };
-
-                foreach (var g in Genes.Skip(geneIndex + 1))
-                {
-                    g.Name = g.Name + 1;
-                }
-                Genes.Insert(geneIndex + 1, newGene);
-            }
-            if (random.Next(chance) == 0)
-            {
-                int sign = 1;
-                if (random.Next(1) == 0) sign = -1;
-                gene.Value += sign;
-            }
-
-
-            if (random.Next(chance) == 0)
-            {
-                var one = random.NextElement(Genes);
-                var two = random.NextElement(Genes);
-
-                var target = random.Next(Size);
-                for (int k = 0; k < random.Next(3); k++)
-                {
-                    Genes.Add( new Gene<int>
+                () => {
+                    var newGene = new Gene<int>
                     {
-                        Name = Genes.Last().Name + 1,
-                        Value = two.Value
-                    });
+                        Name = gene.Name + 1,
+                        Value = gene.Value
+                    };
+
+                    foreach (var g in Genes.Skip(geneIndex + 1))
+                    {
+                        g.Name = g.Name + 1;
+                    }
+                    Genes.Insert(geneIndex + 1, newGene);
+                },
+                () => {
+                    int sign = 1;
+                    if (random.Next(1) == 0) sign = -1;
+                    gene.Value += sign;
+                },
+                () => {
+                    var one = random.NextElement(Genes);
+                    var two = random.NextElement(Genes);
+
+                    var target = random.Next(Size);
+                    for (int k = 0; k < random.Next(3); k++)
+                    {
+                        Genes.Add( new Gene<int>
+                        {
+                            Name = Genes.Last().Name + 1,
+                            Value = two.Value
+                        });
+                    }
+                },
+                () => {
+                    int num = Size / 10;
+                    int start = random.Next(Size - (num ));
+                    int target = random.Next(Size - (num ));
+
+                    var section = Genes.GetRange(start, num);
+                    var newSection = section.Select(g => new Gene<int> { Name = g.Name, Value = g.Value});
+
+                    foreach (var g in Genes.Skip(target))
+                    {
+                        g.Name = g.Name + num;
+                    }
+
+                    Genes.InsertRange(target, newSection);
+                },
+                () => {
+                    Genes.Remove(gene);
                 }
-            }
-
                 
-            if (random.Next(chance) == 0)
-            {
-                int num = Size / 10;
-                int start = random.Next(Size - (num ));
-                int target = random.Next(Size - (num ));
+            };
 
-                var section = Genes.GetRange(start, num);
-                var newSection = section.Select(g => new Gene<int> { Name = g.Name, Value = g.Value});
 
-                foreach (var g in Genes.Skip(target))
-                {
-                    g.Name = g.Name + num;
-                }
+            random.NextElement(mutators)();
 
-                Genes.InsertRange(target, newSection);
-
-            
-            }
-                
-            if(random.Next(chance * chance) == 0)
-            {
-                Genes.Remove(gene);
-            }
         }
     }
 
